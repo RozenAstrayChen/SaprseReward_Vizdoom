1ffdd73427c0f4804df6e7e076ba5924e7541ce6
RozenAstrayChen <rozendhyan@gmail.com>
2019-02-10 04:13:27 +0800

    update some env bug

--- my_way_home/envs.py
+++ my_way_home/envs.py
@@ -255,11 +255,9 @@ class DoomEnvironment(Process):
         
 
     def pre_proc(self, X):
-        x = cv2.cvtColor(X, cv2.COLOR_RGB2GRAY)
-        x = cv2.resize(x, (self.h, self.w))
-        #x = cv2.resize(X, (self.h, self.w), interpolation=cv2.INTER_LINEAR)
-        #x = np.dot(x[..., :3], [0.299, 0.587, 0.114])
-  
+        x = np.array(Image.fromarray(X).convert('L')).astype('float32')
+        x = cv2.resize(x, (84, 84))
+
         return x
     
     def get_init_state(self, s):
